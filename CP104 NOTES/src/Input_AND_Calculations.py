6a33970d067bda65212fda804afe13ea7de7f7a9
mohamed223567 <131572320+mohamed223567@users.noreply.github.com>
2023-09-16 15:01:50 -0400

    ee

---
+++ CP104 NOTES/src/Input_AND_Calculations.py
@@ -0,0 +1,71 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Mohamed
+ID:      169072233
+Email:   alda2233@mylaurier.ca
+__updated__ = "2023-09-11"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+# string input
+# have an "f" infront to add variable in print line
+name = input("enter your name")
+print(f"your name is {name}")
+
+# int input
+number = int(input("Enter number: "))
+print(number * 2)
+
+# decimal input
+decimal = float(input("enter a decimal"))
+
+
+# CALCULATIONS
+number = int(input("enter a value"))
+value = number + 10
+print(f"your value is {value}")
+
+var1 = 50
+var2 = 25
+answer = var1 + var2
+print(f"va1 + va2 -> {var1} + {var2} -> {answer}")
+
+answer = var1 - var2
+print(f"va1 - va2 -> {var1} - {var2} -> {answer}")
+
+answer = var1 * var2
+print(f"va1 * va2 -> {var1} * {var2} -> {answer}")
+
+answer = var1 / var2
+print(f"va1 / va2 -> {var1} / {var2} -> {answer}")  # / float division
+
+answer = var1 // var2
+# // integer division(no decimal)
+print(f"va1 // va2 -> {var1} // {var2} -> {answer}")
+
+answer = var1 % var2
+# %(modulus operator) gives the remainder of the division
+print(f"va1 % va2 -> {var1} % {var2} -> {answer}")
+
+answer = var1 ** var2
+print(f"va1 ** va2 -> {var1} ** {var2} -> {answer}")  # ** means exponentiation
